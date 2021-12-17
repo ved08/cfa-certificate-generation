ad238975c8e406a122155587df397738da1c7a0b
Vedvardhan Gyanmote <vedvardhan.g@gmail.com>
2021-12-17 22:36:02 +0530

    Added / route

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ app.use(express.json())
 app.use(cors())
 
 let uid;
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the backend of CFA certificate generator. Frontend is deployed at https://cfa-certificate-generation.web.app/")
+})
+
 app.post("/1", (req, res) => {
     uid = Date.now()
     const { name } = req.body
@@ -23,11 +28,11 @@ app.post("/2", (req, res) => {
 
 })
 
-app.get(`/certificate/bcc/:nonce`, (req, res) => {
+app.get(`/ certificate / bcc /: nonce`, (req, res) => {
     console.log("UID is:(from GET 1) ", req.params.nonce)
     res.sendFile(path.join(__dirname, "/certs/bcc_processed.jpg"))
 })
-app.get(`/certificate/dlc/:nonce`, (req, res) => {
+app.get(`/ certificate / dlc /: nonce`, (req, res) => {
     console.log("UID is:(from GET 2) ", req.params.nonce)
     res.sendFile(path.join(__dirname, "/certs/dlc_processed.jpg"))
 })
@@ -35,17 +40,17 @@ app.get(`/certificate/dlc/:nonce`, (req, res) => {
 const processImage = async (name, res, filename, modifiedFileName) => {
     name = name.split(" ").map(e => e[0].toUpperCase() + e.split("").splice(1).join("")).join(" ")
     console.log("UID is: ", uid)
-    await Jimp.read(path.join(__dirname, `/certs/${filename}.jpg`))
+    await Jimp.read(path.join(__dirname, `/ certs / ${filename}.jpg`))
         .then(async image => {
             const font = await Jimp.loadFont(path.join(__dirname, "segoepr/segoepr.fnt"));
             image.print(font, 440, 330, name)
-            await image.writeAsync(path.join(__dirname, `/certs/${modifiedFileName}.jpg`))
+            await image.writeAsync(path.join(__dirname, `/ certs / ${modifiedFileName}.jpg`))
             res.end(`http://localhost:5000/certificate/${filename}/${uid}`)
         })
         .catch(err => console.log(err));
 
 }
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("listening in port 5000")
 })
